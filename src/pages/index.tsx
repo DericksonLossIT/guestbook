8b88dbc5d4857625750526cd8ded12d885773439
DericksonLossIT <dericksonlossit@gmail.com>
2022-09-04 23:45:33 -0300

    implements authentication via Next Auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,27 @@
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const Home = () => {
+    const { data: session, status } = useSession();
+
+    if (status === "loading") {
+        return <main>Loading...</main>;
+    }
+
     return (
         <main>
-          <h1>Guestbook</h1>
+            <h1>Guestbook</h1>
+
+            {session ? (
+                <div>
+                    <p>hi {session.user?.name}</p>
+
+                    <button onClick={() => signOut()}>Logout</button>
+                </div>
+            ) : (
+                <button onClick={() => signIn("discord")}>
+                    Login with Discord
+                </button>
+            )}
         </main>
     );
 };
